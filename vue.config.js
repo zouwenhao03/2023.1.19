5313c0ff3b57a4edf18f4af049939325bb1ae8fc
zou <13588824132@163.com>
2023-01-19 17:35:16 +0800

    sssss

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: './',
+    outputDir: 'dist', // dist
+    assetsDir: 'static',
+    lintOnSave: process.env.NODE_ENV === 'development',
+    productionSourceMap: false
+}
